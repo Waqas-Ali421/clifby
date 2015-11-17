82b07fdb2dbdd23514010339807b302e4c88a3ce
ericsong <regonics@gmail.com>
2015-11-17 05:31:43 -0500

    fleshed out chrome extension skeleton

--- getnames.js
+++ getnames.js
@@ -1,3 +1,5 @@
+var elements = document.getElementsByClassName('fsl fwb fcb');
+
 for(var i = 0; i < elements.length; i++) {
   var element = elements[i];
   console.log(element.getElementsByTagName('a')[0].innerText);
