5d9a4a65c740545c16412dba10ab8aef6bc78b2c
ericsong <regonics@gmail.com>
2015-11-24 23:36:32 -0500

    display items in popup

--- popup.js
+++ popup.js
@@ -1,5 +1,11 @@
 console.log('helloooo');
 
 chrome.runtime.sendMessage({payload: 'nameRequest'}, function(names) {
-    console.log(names);  
+    console.log(names);
+    for(var i = 0; i < names.length; i++) {
+        var name = names[i];
+        var newItem = document.createElement("li");
+        newItem.innerHTML = name;
+        element.getElementsByClassName('list')[0].appendChild(newItem);
+    }
 })
--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var elements = document.getElementsByClassName('fsl fwb fcb');
 for(var i = 0; i < elements.length; i++) {
   var element = elements[i];
   names.push(element.getElementsByTagName('a')[0].innerText);
+
 }
 
 function send() {
